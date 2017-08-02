05118b528c4dcf5b9babe147966ada3711da11ba
valentinCo <eersel-val@hotmail.com>
2017-08-02 12:18:45 +0200

    exercice

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-console.log("Bonjour");
-
-
-
-
-
 var object = {
-	Prenom: "Valentin",
+	Prenom: "",
 	Nom: "",
 	Age: ""
 }
 
-
-
-
 $(".send").click(function() {
-var first;
-var last;
-var age;
+
 
 	object.Prenom =  $("#first").val();
 	object.Nom = $("#last").val();
@@ -27,7 +16,15 @@ var age;
 	$(".ici").html("Vous vous appelez " + (object.Prenom) +" "+ (object.Nom) +" "+"et vous avez " + (object.Age)+" ans");
 
 });
+row = $("#tableau").html("<th class='donnee1'>Prenom </th><th class='donnee2'>Nom </th class='donnee3'><th>Age </th>");
+$(".array").click(function(){
+	object.Prenom =  $("#first").val();
+	object.Nom = $("#last").val();
+	object.Age = $("#age").val();
 
+	$("#tableau").append("<tr><td class ='donnee1'>" + object.Prenom +"</td><td class='donnee2'>"+ object.Nom +"</td><td class='donnee3'>" + object.Age + "</td></tr>");
+
+});
 
 
 
